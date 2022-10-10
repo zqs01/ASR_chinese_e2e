dfa48fa1f58510f096515a52f937f9073cdadb33
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-10 11:05:57 +0000

    Adding tarfile member sanitization to extractall()

--- data/extract_aishell1.py
+++ data/extract_aishell1.py
@@ -15,7 +15,26 @@ def extract_aishell1(raw_tgz, to_dir):
     folder = raw_tgz[:-4] + '/wav/'
     for i in tqdm(os.listdir(folder)):
         with tarfile.open(folder + i) as reader:
-            reader.extractall(folder)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(reader, folder)
         os.remove(os.path.join(folder, i))
     print(f'done')
 
